4b3071601474c1c20e06c2143feccf3ef4e10238
ignasmat3 <ignasmat1202@gmail.com>
2024-12-20 07:23:08 +0200

    Pabaigtas frontendas, bandoma bus db kelti i serveri

--- djangoProject2/wsgi.py
+++ djangoProject2/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elevate.settings')
 
 application = get_wsgi_application()
+
+app = application
