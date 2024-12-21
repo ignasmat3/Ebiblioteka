5f343a20c206d312bd94678b9c2837249dcd1b84
ignasmat3 <ignasmat1202@gmail.com>
2024-12-21 22:10:42 +0200

    Pabaigtas frontendas, bandoma bus db kelti i serveri

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -112,6 +112,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    # CORS middleware must be high up
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,7 +122,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoProject2.urls'
